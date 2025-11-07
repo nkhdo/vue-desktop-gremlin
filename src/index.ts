3e9f1b5d2319606708cbb1e6efb161fdb5cc1deb
Hoang Do <nkhdo94@gmail.com>
2025-11-07 22:35:20 +0700

    misc: add ts type to build

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import DesktopGremlin from './components/DesktopGremlin.vue'
 
 // Export component
 export { DesktopGremlin }
-export default DesktopGremlin
 
 // Export types
 export type { CharacterName, CharacterConfig, Position, Velocity } from './types/character'
